78caa1a5cba96c96c6ac5da1ef43db29838dc05a
Vllaadd <vlladdiimiir@gmail.com>
2022-11-06 06:01:33 -0500

    links and routes created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div>
-      <p>Bookshelf in making.</p>
-    </div>>
+      <h1>React Router</h1>
+      <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/bookshelf">Bookshelf</Link>
+      </nav>
+      <Routes>
+        <Route index element={<Home />}/>
+        <Route path="home" element={<Home />}/>
+        <Route path="bookshelf" element={<Bookshelf />}/>
+        <Route path="*" element={<NoMatch />}/>
+      </Routes>
+    </div>
   );
 }
 
