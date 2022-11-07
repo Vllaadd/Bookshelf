7403a3f88b47d9a46b145a332b014ed92e824871
Vllaadd <vlladdiimiir@gmail.com>
2022-11-07 05:06:59 -0500

    index route and nomatch comp added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/navigation";
+import Home from "./components/home";
+import Books from "./components/books";
 
 const App = () => {
   return (
     <>
-    <h1>React Router</h1>
-    <Navigation />
-    <Routes>
-      <Route path="home" element={<Home/>}/>
-      <Route path="users" element={<Books/>}/>
-      <Route />
-    </Routes>
+      <h1>React Router</h1>
+      <Navigation />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="books" element={<Books />} />
+        <Route path="*" element={<NoMatch />}/>
+        <Route />
+      </Routes>
     </>
   );
- };
+};
 
- export default App;
+const NoMatch = () => {
+  return (
+    <p>There's nothing here: 404!</p>;
+  )
+}
+
+export default App;
 
