75d5da898b2b604b5b6760ad0a805c85a1f7aa09
Vllaadd <vlladdiimiir@gmail.com>
2022-11-07 04:18:58 -0500

    two components created

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {Routes, Route} from "react-router-dom";
 import Navigation from "./components/navigation";
 
 const App = () => {
@@ -5,6 +6,11 @@ const App = () => {
     <>
     <h1>React Router</h1>
     <Navigation />
+    <Routes>
+      <Route path="home" element={<Home/>}/>
+      <Route path="users" element={<Books/>}/>
+      <Route />
+    </Routes>
     </>
   );
  };
---
+++ src/components/books.js
@@ -0,0 +1,9 @@
+const Books =()=>{
+    return(
+        <main>
+            <h2>Books</h2>
+        </main>
+    )
+}
+
+export default Books;
