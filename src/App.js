d2f80a044d85625d82c717fbf9a196847c3808c4
Vllaadd <vlladdiimiir@gmail.com>
2022-11-10 04:43:09 -0500

    all files in one yet map still wont run

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import * as React from "react";
 import {Routes, 
         Route,
+        Link,
+        Outlet,
+        useParams,
       useNavigate } from "react-router-dom";
-import Navigation from "./components/navigation";
-import Home from "./components/home";
-import Books from "./components/books";
-import Book from "./components/book"
 
 const App = () => {
   const navigate = useNavigate();
@@ -15,21 +14,18 @@ const App = () => {
     {id:"2", fullName:"Book Two"},
     {id:"3", fullName:"Book Three"}
   ]);
-
-  const handleRemoveBook = (bookId) =>{
+  const handleRemoveBook = (bookId) => {
     setBooks((state) => state.filter((book) => book.id !== bookId));
-    navigate("/books");
+    navigate("/user");
   };
-  
-
   return (
     <>
-      <Navigation />
+      <Navigation/>
       <Routes>
         <Route index element={<Home />} />
         <Route path="home" element={<Home />} />
         <Route path="books" element={<Books books={books}/>}>
-          <Route path=":bookId" element={<Book onRemoveBook={handleRemoveBook} />} />
+          <Route path=":bookId" element={<Book onRemoveBook={handleRemoveBook}/>} />
         </Route>
         <Route path="*" element={<NoMatch />}/>
         <Route />
@@ -38,11 +34,55 @@ const App = () => {
   );
 };
 
+const Navigation = () =>{
+  return(
+    <nav>
+      <Link to="/home">Home</Link>
+      <Link to="/books">Books</Link>
+    </nav>
+  )
+}
+
 const NoMatch = () => {
   return (
     <p>There's nothing here: 404!</p>
   )
 }
+
+const Home = () =>{
+  return (
+      <main>
+          <h2>Home</h2>
+      </main>
+  )
+}
+
+const Books =(books)=>{
+  return(
+      <main>
+          <h2>Books</h2>
+          <ul>
+              {books.map((book) =>(
+                  <li key={book.id}>
+                      <Link to={book.id}>{book.fullName}</Link>
+                  </li>
+              ))}
+          </ul>
+          <Outlet />
+      </main>
+  )
+}
+
+const Book = ({onRemoveBook}) =>{
+  const { bookId } = useParams();
+  return(
+      <>
+      <h2>Book: {bookId}</h2>
+      <button type="button" onClick={() => onRemoveBook(bookId)}>Remove</button>
+      <Link to="/books">Back to Books</Link>
+      </>
+  );
+};
 
 export default App;
 
