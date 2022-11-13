def81e91f85d8b5a5504e6cec13638e4b43454b9
Vllaadd <vlladdiimiir@gmail.com>
2022-11-13 03:24:32 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const App = () => {
    {id: "2", title:"The Song of Enchantment"},
    {id: "3", title:"We are clowns"},
    {id: "4", title:"Cockroaches"},
-
   ]);
   const handleRemoveBook = (bookId) => {
     setBooks((state) => state.filter((book) => book.id !== bookId));
